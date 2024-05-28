ff2fe4ae3b3d3747792a8f7bee950e7bbfa1bfda
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-29 00:53:22 +0500

    File of q127

---
+++ q127.js
@@ -0,0 +1,10 @@
+//Convert a traditional function expression to an arrow function.
+// Traditional function expression
+var traditionalFnction = function (a, b) {
+    return a + b;
+};
+// Converted to arrow function
+var arrowFunction = function (a, b) { return a + b; };
+console.log(traditionalFnction(5, 4)); // Outputs : 9
+console.log(arrowFunction(4, 6)); //  Outputs :10
+// Demonstrates the conversion of a traditional function expression to an arrow function.
---
+++ q127.ts
@@ -0,0 +1,12 @@
+//Convert a traditional function expression to an arrow function.
+
+// Traditional function expression
+const traditionalFnction = function (a, b) {
+    return a + b;
+};
+  // Converted to arrow function
+const arrowFunction = (a,b) => a + b;
+console.log(traditionalFnction(5, 4));      // Outputs : 9
+console.log(arrowFunction(4,6));             //  Outputs :10
+
+  // Demonstrates the conversion of a traditional function expression to an arrow function.
